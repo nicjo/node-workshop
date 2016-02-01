52e222ef7d4167be489379b19d3a7bd607da62e4
nicjo <nicjorgensen@yahoo.ca>
2016-02-01 21:02:00 +0000

    ISS position rounded values

--- iss.js
+++ iss.js
@@ -2,6 +2,7 @@ var request = require('request');
 request('http://api.open-notify.org/iss-now.json', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var issInfo = JSON.parse(body);
-        console.log(issInfo);
+        console.log("the ISS position is: " + Math.round(issInfo.iss_position.latitude * 100) / 100 + " latitude and " + Math.round(issInfo.iss_position.longitude * 100) / 100 + " longitude");
   }
 })
+
