817ca626a42b60af2eba8cbd1cc5ad2d140aed70
nicjo <nicjorgensen@yahoo.ca>
2016-02-02 19:09:31 +0000

    fixed missing/mis-placed file

---
+++ number-guessing-game.js
@@ -0,0 +1,80 @@
+ var prompt = require('prompt');
+ 
+ var numberToGuess = Math.floor(Math.random() * 10)
+ console.log(numberToGuess)
+
+ prompt.start();
+
+ prompt.get("guess", function(err, result1) {
+          var userGuess1 = result1.guess
+
+          if (parseInt(userGuess1) === numberToGuess) {
+             console.log("You win on the first try!");
+             return;
+          
+          } else {   
+                   prompt.get("guess", function(err, result2) {
+                   var userGuess2 = result2.guess
+                         
+                         if (parseInt(userGuess2) === numberToGuess) {
+                            console.log("You got it on the second try!");
+                            return;
+                          
+                         } else {
+            
+                                  prompt.get('guess', function (err, result3) {
+                                  var userGuess3 = result3.guess
+                      
+                                      if (parseInt(userGuess3) === numberToGuess) {
+                                         console.log("Finally!");
+                                         return;
+                                     
+                                      } else {
+                                              console.log("Maybe next time. Here are the numbers you tried: " + userGuess1 + ", " + userGuess2 + " and "+ userGuess3);
+                                              return;
+                                      }
+                                  });
+                                 }
+                    });
+
+                 }
+ });
+ 
+ 
+ // var prompt = require('prompt');
+ 
+ // var numberToGuess = Math.floor(Math.random() * 10)
+ // console.log(numberToGuess)
+
+ // prompt.start();
+
+ // prompt.get("guess", function(err, result1) {
+ //          var userGuess1 = result1.guess
+
+ //          if (userGuess1 === numberToGuess) {
+ //             console.log("You win on the first try!");
+            
+ //          } else {
+ //           return;
+ //          }
+ // });
+ 
+ 
+ // var prompt = require('prompt');
+ 
+ // var numberToGuess = Math.floor(Math.random() * 10)
+
+ // console.log(numberToGuess)
+ // // prompt.start();
+
+ // prompt.get("guess", function(err, result1) {
+  
+ //          if (parseInt(result1.guess) === numberToGuess) {
+ //             console.log("You win on the first try!");
+      
+ //          } else {
+ //           console.log("this ain't working")
+ //          }
+ // });
+ 
+ 
